497a4ab25d57ed4ec769bd7297c2679a77eeeeda
Mohd-Ghayas-Ashraf <ghayas.mohd@gmail.com>
2025-06-17 19:06:21 +0530

    Fixed Route Path Again

--- src/UiStarterServiceProvider.php
+++ src/UiStarterServiceProvider.php
@@ -9,12 +9,12 @@ class UiStarterServiceProvider extends ServiceProvider
     public function boot()
     {
 
-        
+
         $this->publishes([
             __DIR__ . '/../resources/views' => resource_path('views'),
             __DIR__ . '/../public/images' => public_path('images'),
-            __DIR__ . '/../routes/apps1-routes.php' => base_path('routes/apps/app1/apps1-routes.php'),
-            
+            __DIR__ . '/../routes/apps1-routes.php' => base_path('routes\apps\app1\apps1-routes.php'),
+
         ], 'starter-files');
 
     }
