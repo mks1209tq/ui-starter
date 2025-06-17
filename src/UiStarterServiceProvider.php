488a83b137576814e4390c96c1f5ebc9d9b587c2
Mohd-Ghayas-Ashraf <ghayas.mohd@gmail.com>
2025-06-17 17:11:55 +0530

    Changed Folder Structure

--- src/UiStarterServiceProvider.php
+++ src/UiStarterServiceProvider.php
@@ -9,7 +9,7 @@ class UiStarterServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->publishes([
-            __DIR__.'/resources/views/welcome.blade.php' => resource_path('views/welcome.blade.php'),
+            __DIR__.'/../resources/views/welcome.blade.php' => resource_path('views/welcome.blade.php'),
         ], 'starter-files');
     }
 
