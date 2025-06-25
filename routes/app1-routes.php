f30122632b4475904393144f417294f77522ce89
Mohd-Ghayas-Ashraf <ghayas.mohd@gmail.com>
2025-06-25 12:51:32 +0530

    Fixed Route Name

--- routes/app1-routes.php
+++ routes/app1-routes.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/apps/app1', function () {
     return view('apps.app1.index');
-})->name('apps1');
+})->name('apps');
 
 Route::get('/people', function () {
     return view('people.index');
